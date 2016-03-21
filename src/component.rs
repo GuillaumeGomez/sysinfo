c69a6d51e19f4c793728f2495ec0af6085c99a73
ggomez <guillaume1.gomez@gmail.com>
2016-03-21 11:46:12 +0100

    Add max and critical temperatures

--- src/component.rs
+++ src/component.rs
@@ -6,14 +6,38 @@
 
 use std::collections::HashMap;
 use std::fs::{File, read_dir};
+use std::fmt::{Debug, Error, Formatter};
 use std::io::Read;
 use std::path::{Path, PathBuf};
 
 pub struct Component {
-    pub temperature: f32, // temperature is in celsius
-    pub label: String,
+    temperature: f32, // temperature is in celsius
+    max: f32, // the highest stored temperature
+    critical: Option<f32>, // the highest temperature before the computer halts
+    label: String,
     input_file: PathBuf,
-    label_file: PathBuf,
+}
+
+impl Debug for Component {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        if let Some(critical) = self.critical {
+            write!(f, "{}: {}°C (max: {}°C / critical: {}°C)",
+                   self.label, self.temperature, self.max, critical)
+        } else {
+            write!(f, "{}: {}°C (max: {}°C)",
+                   self.label, self.temperature, self.max)
+        }
+    }
+}
+
+fn get_file_line(file: &Path) -> Option<String> {
+    let mut reader = String::new();
+    if let Ok(mut f) = File::open(file) {
+        f.read_to_string(&mut reader).unwrap();
+        Some(reader)
+    } else {
+        None
+    }
 }
 
 fn append_files(components: &mut Vec<Component>, folder: &Path) {
@@ -22,7 +46,8 @@ fn append_files(components: &mut Vec<Component>, folder: &Path) {
         for entry in dir {
             if let Ok(entry) = entry {
                 let entry = entry.path();
-                if entry.is_dir() || !entry.file_name().unwrap().to_str().unwrap_or("").starts_with("temp") {
+                if entry.is_dir() || !entry.file_name().unwrap().to_str()
+                                           .unwrap_or("").starts_with("temp") {
                     continue;
                 }
                 if let Some(entry) = entry.file_name() {
@@ -48,10 +73,26 @@ fn append_files(components: &mut Vec<Component>, folder: &Path) {
                 if found_label.is_some() && found_input.is_some() {
                     let mut p_label = folder.to_path_buf();
                     let mut p_input = folder.to_path_buf();
+                    let mut p_crit = folder.to_path_buf();
+                    let mut p_max = folder.to_path_buf();
 
                     p_label.push(&format!("temp{}_label", key));
                     p_input.push(&format!("temp{}_input", key));
-                    components.push(Component::new(p_label.as_path(), p_input.as_path()));
+                    p_max.push(&format!("temp{}_max", key));
+                    p_crit.push(&format!("temp{}_crit", key));
+                    let content = get_file_line(p_label.as_path()).unwrap();
+                    let label = content.replace("\n", "");
+                    let max = if let Some(max) = get_file_line(p_max.as_path()) {
+                        Some(max.replace("\n", "").parse::<f32>().unwrap() / 1000f32)
+                    } else {
+                        None
+                    };
+                    let crit = if let Some(crit) = get_file_line(p_crit.as_path()) {
+                        Some(crit.replace("\n", "").parse::<f32>().unwrap() / 1000f32)
+                    } else {
+                        None
+                    };
+                    components.push(Component::new(label, p_input.as_path(), max, crit));
                     break
                 }
             }
@@ -66,7 +107,8 @@ impl Component {
             for entry in dir {
                 if let Ok(entry) = entry {
                     let entry = entry.path();
-                    if !entry.is_dir() || !entry.file_name().unwrap().to_str().unwrap_or("").starts_with("hwmon") {
+                    if !entry.is_dir() || !entry.file_name().unwrap().to_str()
+                                                .unwrap_or("").starts_with("hwmon") {
                         continue;
                     }
                     append_files(&mut ret, &entry);
@@ -77,25 +119,40 @@ impl Component {
         ret
     }
 
-    pub fn new(label_path: &Path, input_path: &Path) -> Component {
+    pub fn new(label: String, input_path: &Path, max: Option<f32>,
+               critical: Option<f32>) -> Component {
         let mut c = Component {
             temperature: 0f32,
-            label: String::new(),
+            label: label,
             input_file: input_path.to_path_buf(),
-            label_file: label_path.to_path_buf(),
+            max: max.unwrap_or(0.0),
+            critical: critical,
         };
         c.update();
         c
     }
 
     pub fn update(&mut self) {
-        let mut f = File::open(self.label_file.as_path()).unwrap();
-        let mut reader = String::new();
-        f.read_to_string(&mut reader).unwrap();
-        self.label = reader.replace("\n", "");
-        let mut f = File::open(self.input_file.as_path()).unwrap();
-        reader = String::new();
-        f.read_to_string(&mut reader).unwrap();
-        self.temperature = reader.replace("\n", "").parse::<f32>().unwrap() / 1000f32;
+        let content = get_file_line(self.input_file.as_path()).unwrap();
+        self.temperature = content.replace("\n", "").parse::<f32>().unwrap() / 1000f32;
+        if self.temperature > self.max {
+            self.max = self.temperature;
+        }
+    }
+
+    pub fn get_label(&self) -> &str {
+        &self.label
+    }
+
+    pub fn get_temperature(&self) -> f32 {
+        self.temperature
+    }
+
+    pub fn get_max(&self) -> f32 {
+        self.max
+    }
+
+    pub fn get_critical(&self) -> Option<f32> {
+        self.critical
     }
 }
