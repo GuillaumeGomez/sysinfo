75bfeb74c45b3c05dad721c38296eb9e49330e64
nate <nateozemon@gmail.com>
2017-02-19 09:52:40 -0800

    Migrating to a universal code base: created enums.rs and place procees status in the file. move Process type to src/process.rs. Done only for macOS. Will do Linux if no objection of moving ahead with the type of changes.

--- src/mac/mod.rs
+++ src/mac/mod.rs
@@ -7,12 +7,12 @@
 pub mod component;
 pub mod disk;
 mod ffi;
-pub mod process;
 pub mod processor;
 pub mod system;
 
 pub use self::component::Component;
 pub use self::disk::{Disk, DiskType};
-pub use self::process::{Process,ProcessStatus};
 pub use self::processor::Processor;
 pub use self::system::System;
+pub use process::Process;
+pub use enums::ProcessStatus;
