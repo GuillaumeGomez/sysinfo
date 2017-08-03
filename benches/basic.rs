b73c42ee25366a562bbb442dffa32715f07613a3
Guillaume Gomez <guillaume1.gomez@gmail.com>
2017-08-03 23:58:51 +0200

    Add benches and fix clippy warning

--- benches/basic.rs
+++ benches/basic.rs
@@ -13,10 +13,55 @@ fn bench_new(b: &mut test::Bencher) {
 }
 
 #[bench]
-fn bench_refresh(b: &mut test::Bencher) {
+fn bench_refresh_all(b: &mut test::Bencher) {
     let mut s = sysinfo::System::new();
 
     b.iter(move || {
         s.refresh_all();
+    });
+}
+
+#[bench]
+fn bench_refresh_system(b: &mut test::Bencher) {
+    let mut s = sysinfo::System::new();
+
+    b.iter(move || {
+        s.refresh_system();
+    });
+}
+
+#[bench]
+fn bench_refresh_processes(b: &mut test::Bencher) {
+    let mut s = sysinfo::System::new();
+
+    b.iter(move || {
+        s.refresh_processes();
+    });
+}
+
+#[bench]
+fn bench_refresh_disks(b: &mut test::Bencher) {
+    let mut s = sysinfo::System::new();
+
+    b.iter(move || {
+        s.refresh_disks();
+    });
+}
+
+#[bench]
+fn bench_refresh_disk_lists(b: &mut test::Bencher) {
+    let mut s = sysinfo::System::new();
+
+    b.iter(move || {
+        s.refresh_disk_list();
+    });
+}
+
+#[bench]
+fn bench_refresh_network(b: &mut test::Bencher) {
+    let mut s = sysinfo::System::new();
+
+    b.iter(move || {
+        s.refresh_network();
     });
 }
