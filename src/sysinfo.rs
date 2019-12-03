1f52533aeebab1b724f7abd7d8b0728c3039e1da
Lonng <heng@lonng.org>
2019-12-03 19:21:12 +0800

    support retrieve system wide configuration list
    Signed-off-by: Lonng <heng@lonng.org>

--- src/sysinfo.rs
+++ src/sysinfo.rs
@@ -91,6 +91,8 @@ pub use traits::{ComponentExt, DiskExt, NetworkExt, ProcessExt, ProcessorExt, Sy
 pub use c_interface::*;
 pub use utils::get_current_pid;
 
+use std::collections::HashMap;
+
 #[cfg(feature = "c-interface")]
 mod c_interface;
 mod common;
@@ -222,6 +224,35 @@ pub struct LoadAvg {
     pub five: f64,
     /// Average load within fifteen minites.
     pub fifteen: f64,
+}
+
+/// Returns system wide configuration
+///
+/// # Note
+///
+/// Current only can be used in operating system mounted `procfs`
+pub fn get_sysctl_list() -> HashMap<String, String> {
+    const DIR: &str = "/proc/sys/";
+    let mut result = HashMap::new();
+    for entry in walkdir::WalkDir::new(DIR) {
+        let entry = match entry {
+            Ok(entry) => entry,
+            _ => continue,
+        };
+
+        let content = match std::fs::read_to_string(entry.path()) {
+            Ok(c) => c,
+            _ => continue,
+        };
+
+        let path = match entry.path().to_str() {
+            Some(p) => p,
+            _ => continue,
+        };
+        let name = path.trim_start_matches(DIR).replace("/", ".");
+        result.insert(name, content.trim().to_string());
+    }
+    result
 }
 
 #[cfg(test)]
