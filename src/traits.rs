f8d766aef47e52961cddd553133b63eeff0b657e
GuillaumeGomez <guillaume1.gomez@gmail.com>
2020-01-27 00:19:25 +0100

    Add code examples

--- src/traits.rs
+++ src/traits.rs
@@ -26,24 +26,79 @@ use std::path::Path;
 /// ```
 pub trait DiskExt {
     /// Returns the disk type.
+    ///
+    /// ```no_run
+    /// use sysinfo::{DiskExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// for disk in s.get_disks() {
+    ///     println!("{:?}", disk.get_type());
+    /// }
+    /// ```
     fn get_type(&self) -> DiskType;
 
     /// Returns the disk name.
+    ///
+    /// ```no_run
+    /// use sysinfo::{DiskExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// for disk in s.get_disks() {
+    ///     println!("{:?}", disk.get_name());
+    /// }
+    /// ```
     fn get_name(&self) -> &OsStr;
 
     /// Returns the file system used on this disk (so for example: `EXT4`, `NTFS`, etc...).
+    ///
+    /// ```no_run
+    /// use sysinfo::{DiskExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// for disk in s.get_disks() {
+    ///     println!("{:?}", disk.get_file_system());
+    /// }
+    /// ```
     fn get_file_system(&self) -> &[u8];
 
     /// Returns the mount point of the disk (`/` for example).
+    ///
+    /// ```no_run
+    /// use sysinfo::{DiskExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// for disk in s.get_disks() {
+    ///     println!("{:?}", disk.get_mount_point());
+    /// }
+    /// ```
     fn get_mount_point(&self) -> &Path;
 
     /// Returns the total disk size, in bytes.
+    ///
+    /// ```no_run
+    /// use sysinfo::{DiskExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// for disk in s.get_disks() {
+    ///     println!("{}", disk.get_total_space());
+    /// }
+    /// ```
     fn get_total_space(&self) -> u64;
 
     /// Returns the available disk size, in bytes.
+    ///
+    /// ```no_run
+    /// use sysinfo::{DiskExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// for disk in s.get_disks() {
+    ///     println!("{}", disk.get_available_space());
+    /// }
+    /// ```
     fn get_available_space(&self) -> u64;
 
     /// Update the disk' information.
+    #[doc(hidden)]
     fn update(&mut self) -> bool;
 }
 
@@ -52,56 +107,183 @@ pub trait ProcessExt {
     /// Create a new process only containing the given information.
     ///
     /// On windows, the `start_time` argument is ignored.
+    #[doc(hidden)]
     fn new(pid: Pid, parent: Option<Pid>, start_time: u64) -> Self;
 
     /// Sends the given `signal` to the process.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessExt, Signal, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// if let Some(process) = s.get_process(1337) {
+    ///     process.kill(Signal::Kill);
+    /// }
+    /// ```
     fn kill(&self, signal: ::Signal) -> bool;
 
     /// Returns the name of the process.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// if let Some(process) = s.get_process(1337) {
+    ///     println!("{}", process.name());
+    /// }
+    /// ```
     fn name(&self) -> &str;
 
     /// Returns the command line.
     // ///
     // /// On Windows, this is always a one element vector.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// if let Some(process) = s.get_process(1337) {
+    ///     println!("{:?}", process.cmd());
+    /// }
+    /// ```
     fn cmd(&self) -> &[String];
 
     /// Returns the path to the process.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// if let Some(process) = s.get_process(1337) {
+    ///     println!("{}", process.exe().display());
+    /// }
+    /// ```
     fn exe(&self) -> &Path;
 
     /// Returns the pid of the process.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// if let Some(process) = s.get_process(1337) {
+    ///     println!("{}", process.pid());
+    /// }
+    /// ```
     fn pid(&self) -> Pid;
 
     /// Returns the environment of the process.
     ///
-    /// Always empty on Windows except for current process.
+    /// Always empty on Windows, except for current process.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// if let Some(process) = s.get_process(1337) {
+    ///     println!("{:?}", process.environ());
+    /// }
+    /// ```
     fn environ(&self) -> &[String];
 
     /// Returns the current working directory.
     ///
     /// Always empty on Windows.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// if let Some(process) = s.get_process(1337) {
+    ///     println!("{}", process.cwd().display());
+    /// }
+    /// ```
     fn cwd(&self) -> &Path;
 
     /// Returns the path of the root directory.
     ///
     /// Always empty on Windows.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// if let Some(process) = s.get_process(1337) {
+    ///     println!("{}", process.root().display());
+    /// }
+    /// ```
     fn root(&self) -> &Path;
 
     /// Returns the memory usage (in KiB).
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// if let Some(process) = s.get_process(1337) {
+    ///     println!("{} KiB", process.memory());
+    /// }
+    /// ```
     fn memory(&self) -> u64;
 
     /// Returns the virtual memory usage (in KiB).
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// if let Some(process) = s.get_process(1337) {
+    ///     println!("{} KiB", process.virtual_memory());
+    /// }
+    /// ```
     fn virtual_memory(&self) -> u64;
 
     /// Returns the parent pid.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// if let Some(process) = s.get_process(1337) {
+    ///     println!("{:?}", process.parent());
+    /// }
+    /// ```
     fn parent(&self) -> Option<Pid>;
 
     /// Returns the status of the processus.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// if let Some(process) = s.get_process(1337) {
+    ///     println!("{:?}", process.status());
+    /// }
+    /// ```
     fn status(&self) -> ProcessStatus;
 
     /// Returns the time of process launch (in seconds).
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// if let Some(process) = s.get_process(1337) {
+    ///     println!("{}", process.start_time());
+    /// }
+    /// ```
     fn start_time(&self) -> u64;
 
-    /// Returns the total CPU usage.
+    /// Returns the total CPU usage (in %).
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// if let Some(process) = s.get_process(1337) {
+    ///     println!("{}%", process.cpu_usage());
+    /// }
+    /// ```
     fn cpu_usage(&self) -> f32;
 }
 
@@ -111,15 +293,51 @@ pub trait ProcessorExt {
     ///
     /// Note: You'll need to refresh it at least twice at first if you want to have a
     /// non-zero value.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessorExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// for processor in s.get_processor_list() {
+    ///     println!("{}%", processor.get_cpu_usage());
+    /// }
+    /// ```
     fn get_cpu_usage(&self) -> f32;
 
     /// Returns this processor's name.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessorExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// for processor in s.get_processor_list() {
+    ///     println!("{}", processor.get_name());
+    /// }
+    /// ```
     fn get_name(&self) -> &str;
 
     /// Returns the processor's vendor id.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessorExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// for processor in s.get_processor_list() {
+    ///     println!("{}", processor.get_vendor_id());
+    /// }
+    /// ```
     fn get_vendor_id(&self) -> &str;
 
     /// Returns the processor's frequency.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessorExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// for processor in s.get_processor_list() {
+    ///     println!("{}", processor.get_frequency());
+    /// }
+    /// ```
     fn get_frequency(&self) -> u64;
 }
 
@@ -130,6 +348,12 @@ pub trait SystemExt: Sized {
     /// the `refresh_` method).
     ///
     /// [`refresh_all`]: #method.refresh_all
+    ///
+    /// ```no_run
+    /// use sysinfo::{System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// ```
     fn new() -> Self {
         let mut s = Self::new_with_specifics(RefreshKind::new());
         s.refresh_disk_list();
@@ -203,6 +427,13 @@ pub trait SystemExt: Sized {
     /// [`refresh_memory`]: SystemExt::refresh_memory
     /// [`refresh_cpu`]: SystemExt::refresh_memory
     /// [`refresh_temperatures`]: SystemExt::refresh_temperatures
+    ///
+    /// ```no_run
+    /// use sysinfo::{System, SystemExt};
+    ///
+    /// let mut s = System::new();
+    /// s.refresh_system();
+    /// ```
     fn refresh_system(&mut self) {
         self.refresh_memory();
         self.refresh_cpu();
@@ -210,31 +441,94 @@ pub trait SystemExt: Sized {
     }
 
     /// Refresh RAM and SWAP usage.
+    ///
+    /// ```no_run
+    /// use sysinfo::{System, SystemExt};
+    ///
+    /// let mut s = System::new();
+    /// s.refresh_memory();
+    /// ```
     fn refresh_memory(&mut self);
 
     /// Refresh CPU usage.
+    ///
+    /// ```no_run
+    /// use sysinfo::{System, SystemExt};
+    ///
+    /// let mut s = System::new();
+    /// s.refresh_cpu();
+    /// ```
     fn refresh_cpu(&mut self);
 
     /// Refresh components' temperature.
+    ///
+    /// ```no_run
+    /// use sysinfo::{System, SystemExt};
+    ///
+    /// let mut s = System::new();
+    /// s.refresh_temperatures();
+    /// ```
     fn refresh_temperatures(&mut self);
 
     /// Get all processes and update their information.
+    ///
+    /// ```no_run
+    /// use sysinfo::{System, SystemExt};
+    ///
+    /// let mut s = System::new();
+    /// s.refresh_processes();
+    /// ```
     fn refresh_processes(&mut self);
 
     /// Refresh *only* the process corresponding to `pid`. Returns `false` if the process doesn't
     /// exist.
+    ///
+    /// ```no_run
+    /// use sysinfo::{System, SystemExt};
+    ///
+    /// let mut s = System::new();
+    /// s.refresh_process(1337);
+    /// ```
     fn refresh_process(&mut self, pid: Pid) -> bool;
 
     /// Refreshes the listed disks' information.
+    ///
+    /// ```no_run
+    /// use sysinfo::{System, SystemExt};
+    ///
+    /// let mut s = System::new();
+    /// s.refresh_disks();
+    /// ```
     fn refresh_disks(&mut self);
 
     /// The disk list will be emptied then completely recomputed.
+    ///
+    /// ```no_run
+    /// use sysinfo::{System, SystemExt};
+    ///
+    /// let mut s = System::new();
+    /// s.refresh_disk_list();
+    /// ```
     fn refresh_disk_list(&mut self);
 
     /// Refresh data network.
+    ///
+    /// ```no_run
+    /// use sysinfo::{System, SystemExt};
+    ///
+    /// let mut s = System::new();
+    /// s.refresh_network();
+    /// ```
     fn refresh_network(&mut self);
 
     /// Refreshes all system, processes and disks information.
+    ///
+    /// ```no_run
+    /// use sysinfo::{System, SystemExt};
+    ///
+    /// let mut s = System::new();
+    /// s.refresh_all();
+    /// ```
     fn refresh_all(&mut self) {
         self.refresh_system();
         self.refresh_processes();
@@ -243,12 +537,39 @@ pub trait SystemExt: Sized {
     }
 
     /// Returns the process list.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// for (pid, process) in s.get_process_list() {
+    ///     println!("{} {}", pid, process.name());
+    /// }
+    /// ```
     fn get_process_list(&self) -> &HashMap<Pid, Process>;
 
     /// Returns the process corresponding to the given pid or `None` if no such process exists.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// if let Some(process) = s.get_process(1337) {
+    ///     println!("{}", process.name());
+    /// }
+    /// ```
     fn get_process(&self, pid: Pid) -> Option<&Process>;
 
     /// Returns a list of process containing the given `name`.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// for process in s.get_process_by_name("htop") {
+    ///     println!("{} {}", process.pid(), process.name());
+    /// }
+    /// ```
     fn get_process_by_name(&self, name: &str) -> Vec<&Process> {
         let mut ret = vec![];
         for val in self.get_process_list().values() {
@@ -260,59 +581,211 @@ pub trait SystemExt: Sized {
     }
 
     /// The first processor in the array is the "main" one (aka the addition of all the others).
+    ///
+    /// ```no_run
+    /// use sysinfo::{ProcessorExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// for processor in s.get_processor_list() {
+    ///     println!("{}%", processor.get_cpu_usage());
+    /// }
+    /// ```
     fn get_processor_list(&self) -> &[Processor];
 
     /// Returns total RAM size in KiB.
+    ///
+    /// ```no_run
+    /// use sysinfo::{System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// println!("{} KiB", s.get_total_memory());
+    /// ```
     fn get_total_memory(&self) -> u64;
 
     /// Returns free RAM size in KiB.
+    ///
+    /// ```no_run
+    /// use sysinfo::{System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// println!("{} KiB", s.get_free_memory());
+    /// ```
     fn get_free_memory(&self) -> u64;
 
     /// Returns used RAM size in KiB.
+    ///
+    /// ```no_run
+    /// use sysinfo::{System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// println!("{} KiB", s.get_used_memory());
+    /// ```
     fn get_used_memory(&self) -> u64;
 
     /// Returns SWAP size in KiB.
+    ///
+    /// ```no_run
+    /// use sysinfo::{System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// println!("{} KiB", s.get_total_swap());
+    /// ```
     fn get_total_swap(&self) -> u64;
 
     /// Returns free SWAP size in KiB.
+    ///
+    /// ```no_run
+    /// use sysinfo::{System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// println!("{} KiB", s.get_free_swap());
+    /// ```
     fn get_free_swap(&self) -> u64;
 
     /// Returns used SWAP size in KiB.
+    ///
+    /// ```no_run
+    /// use sysinfo::{System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// println!("{} KiB", s.get_used_swap());
+    /// ```
     fn get_used_swap(&self) -> u64;
 
     /// Returns components list.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ComponentExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// for component in s.get_components_list() {
+    ///     println!("{}: {}°C", component.get_label(), component.get_temperature());
+    /// }
+    /// ```
     fn get_components_list(&self) -> &[Component];
 
     /// Returns disks' list.
+    ///
+    /// ```no_run
+    /// use sysinfo::{DiskExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// for disk in s.get_disks() {
+    ///     println!("{:?}", disk.get_name());
+    /// }
+    /// ```
     fn get_disks(&self) -> &[Disk];
 
     /// Returns network data.
+    ///
+    /// ```no_run
+    /// use sysinfo::{NetworkExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// let network = s.get_network();
+    /// println!("in: {}, out: {}", network.get_income(), network.get_outcome());
+    /// ```
     fn get_network(&self) -> &NetworkData;
 
     /// Returns system uptime.
+    ///
+    /// ```no_run
+    /// use sysinfo::{System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// println!("{}", s.get_uptime());
+    /// ```
     fn get_uptime(&self) -> u64;
 
     /// Returns the system load average value.
+    ///
+    /// ```no_run
+    /// use sysinfo::{System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// let load_avg = s.get_load_average();
+    /// println!(
+    ///     "one minute: {}%, five minutes: {}%, fifteen minutes: {}%",
+    ///     load_avg.one,
+    ///     load_avg.five,
+    ///     load_avg.fifteen,
+    /// );
+    /// ```
     fn get_load_average(&self) -> LoadAvg;
 }
 
 /// Getting volume of incoming and outgoing data.
 pub trait NetworkExt {
     /// Returns the number of incoming bytes.
+    ///
+    /// ```no_run
+    /// use sysinfo::{NetworkExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// let network = s.get_network();
+    /// println!("in: {}", network.get_income());
+    /// ```
     fn get_income(&self) -> u64;
 
     /// Returns the number of outgoing bytes.
+    ///
+    /// ```no_run
+    /// use sysinfo::{NetworkExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// let network = s.get_network();
+    /// println!("out: {}", network.get_outcome());
+    /// ```
     fn get_outcome(&self) -> u64;
 }
 
 /// Getting a component temperature information.
 pub trait ComponentExt {
     /// Returns the component's temperature (in celsius degree).
+    ///
+    /// ```no_run
+    /// use sysinfo::{ComponentExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// for component in s.get_components_list() {
+    ///     println!("{}°C", component.get_temperature());
+    /// }
+    /// ```
     fn get_temperature(&self) -> f32;
+
     /// Returns the maximum temperature of this component.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ComponentExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// for component in s.get_components_list() {
+    ///     println!("{}°C", component.get_max());
+    /// }
+    /// ```
     fn get_max(&self) -> f32;
+
     /// Returns the highest temperature before the computer halts.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ComponentExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// for component in s.get_components_list() {
+    ///     println!("{:?}°C", component.get_critical());
+    /// }
+    /// ```
     fn get_critical(&self) -> Option<f32>;
+
     /// Returns component's label.
+    ///
+    /// ```no_run
+    /// use sysinfo::{ComponentExt, System, SystemExt};
+    ///
+    /// let s = System::new();
+    /// for component in s.get_components_list() {
+    ///     println!("{}", component.get_label());
+    /// }
+    /// ```
     fn get_label(&self) -> &str;
 }
