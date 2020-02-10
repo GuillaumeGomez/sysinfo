924f34ddc02df0464cd744de86d485e61eba97a6
GuillaumeGomez <guillaume1.gomez@gmail.com>
2020-02-10 22:07:48 +0100

    Add tests to be sure that SystemExt::refresh_process adds the process if it isn't listed yet

--- tests/process.rs
+++ tests/process.rs
@@ -6,12 +6,12 @@
 
 extern crate sysinfo;
 
+#[cfg(not(windows))]
+use sysinfo::ProcessExt;
+use sysinfo::SystemExt;
+
 #[test]
 fn test_process() {
-    #[cfg(not(windows))]
-    use sysinfo::ProcessExt;
-    use sysinfo::SystemExt;
-
     let mut s = sysinfo::System::new();
     assert_eq!(s.get_processes().len(), 0);
     s.refresh_processes();
@@ -21,4 +21,12 @@ fn test_process() {
         .get_processes()
         .values()
         .any(|p| p.exe().to_str().unwrap_or_else(|| "").len() != 0));
+}
+
+#[test]
+fn test_process_refresh() {
+    let mut s = sysinfo::System::new();
+    assert_eq!(s.get_processes().len(), 0);
+    s.refresh_process(sysinfo::get_current_pid().expect("failed to get current pid"));
+    assert_eq!(s.get_process(sysinfo::get_current_pid().expect("failed to get current pid")).is_some(), true);
 }
