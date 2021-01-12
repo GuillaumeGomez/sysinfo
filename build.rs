ac414aef59b4e9c2efb5aa18c7acad9be10bfee8
Brandon W Maister <bwm@materialize.io>
2021-01-12 16:16:28 -0500

    Correct build for cross-compilation
    The build target of the build script is not the same as the build target of the
    final lib. Using the *runtime* environment variable (as opposed to the cfg var
    or `env!()`) determines the actual target of the library.
    
    For example, if you are running `cargo build --target=x86_64-apple-darwin` on
    Linux then the target for the `build.rs` script *must* be linux -- because the
    script is executed on the current machine -- but the target of the `sysinfo`
    lib is the `--target` parameter.

--- build.rs
+++ build.rs
@@ -1,25 +1,25 @@
-#[cfg(target_os = "macos")]
 extern crate cc;
 
-#[cfg(target_os = "macos")]
 fn main() {
+    let is_apple = std::env::var("TARGET")
+        .map(|t| t.contains("-apple"))
+        .unwrap_or(false);
     let is_ios = std::env::var("CARGO_CFG_TARGET_OS")
         .map(|s| s == "ios")
         .unwrap_or(false);
 
-    if !is_ios {
-        cc::Build::new()
-            .file("src/apple/macos/disk.m")
-            .compile("disk");
-        // DiskArbitration is not available on iOS: https://developer.apple.com/documentation/diskarbitration
-        println!("cargo:rustc-link-lib=framework=DiskArbitration");
-        // IOKit is not available on iOS: https://developer.apple.com/documentation/iokit
-        println!("cargo:rustc-link-lib=framework=IOKit");
+    if is_apple {
+        if !is_ios {
+            cc::Build::new()
+                .file("src/apple/macos/disk.m")
+                .compile("disk");
+            // DiskArbitration is not available on iOS: https://developer.apple.com/documentation/diskarbitration
+            println!("cargo:rustc-link-lib=framework=DiskArbitration");
+            // IOKit is not available on iOS: https://developer.apple.com/documentation/iokit
+            println!("cargo:rustc-link-lib=framework=IOKit");
+        }
+
+        println!("cargo:rustc-link-lib=framework=Foundation");
+        println!("cargo:rustc-link-lib=framework=CoreFoundation");
     }
-
-    println!("cargo:rustc-link-lib=framework=Foundation");
-    println!("cargo:rustc-link-lib=framework=CoreFoundation");
 }
-
-#[cfg(not(target_os = "macos"))]
-fn main() {}
