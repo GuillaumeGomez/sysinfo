93d05b25f206b704c2b2663332228f64895676a1
Ricky (deg4usser) <ricky@hosfelt.io>
2021-02-07 19:26:33 -0500

    fix linux implementation

--- src/linux/system.rs
+++ src/linux/system.rs
@@ -492,10 +492,10 @@ impl SystemExt for System {
 
     fn get_long_os_version(&self) -> Option<String> {
         #[cfg(target_os = "android")]
-        let long_name = format!("Android {} {}", get_os_version(), get_name());
+        let long_name = format!("Android {} {}", self.get_os_version().unwrap_or_default(), self.get_name().unwrap_or_default());
 
         #[cfg(target_os = "linux")]
-        let long_name = format!("Linux {} {}", get_os_version(), get_name());
+        let long_name = format!("Linux {} {}", self.get_os_version().unwrap_or_default(), self.get_name().unwrap_or_default());
 
         Some(long_name)
     }
