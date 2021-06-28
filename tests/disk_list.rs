f938ba1383dfb23622e624ae207abeaf4d921091
Guillaume Gomez <guillaume1.gomez@gmail.com>
2021-06-28 22:07:51 +0200

    Update disk test

--- tests/disk_list.rs
+++ tests/disk_list.rs
@@ -10,6 +10,9 @@ fn test_disks() {
 
     if sysinfo::System::IS_SUPPORTED {
         let s = sysinfo::System::new_all();
-        assert!(!s.disks().is_empty());
+        // If we don't have any physical core present, it's very likely that we're inside a VM...
+        if s.physical_core_count().unwrap_or_default() > 0 {
+            assert!(!s.disks().is_empty());
+        }
     }
 }
