871fafbb8b2b3257bb94f0c6c91ae207a916efcf
Guillaume Gomez <guillaume.gomez@huawei.com>
2021-11-15 12:25:19 +0100

    Add test to ensure that the license used in the headers is the correct one

--- tests/network.rs
+++ tests/network.rs
@@ -1,8 +1,4 @@
-//
-// Sysinfo
-//
-// Copyright (c) 2020 Guillaume Gomez
-//
+// Take a look at the license at the top of the repository in the LICENSE file.
 
 // This test is used to ensure that the processors are loaded whatever the method
 // used to initialize `System`.
