b666544d9d36af52f473b5ac2e2e42eae6539057
William Venner <william@venner.io>
2022-01-18 00:28:37 +0000

    Fix missing import

--- src/unknown/disk.rs
+++ src/unknown/disk.rs
@@ -1,6 +1,6 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
-use crate::{DiskExt, DiskType};
+use crate::{DiskExt, DiskType, DiskUsage};
 
 use std::{ffi::OsStr, path::Path};
 
