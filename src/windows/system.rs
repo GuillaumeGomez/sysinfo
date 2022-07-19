GuillaumeGomez/sysinfo

commit 87773f69e3a128d3071d9d1702fc127083adf72e

Use `saturating_*` for mathematical operations

src/windows/system.rs
@@ -73,7 +73,7 @@ unsafe impl<T> Sync for Wrap<T> {}
 
 unsafe fn boot_time() -> u64 {
     match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-        Ok(n) => n.as_secs() - GetTickCount64() / 1000,
+        Ok(n) => n.as_secs().saturating_sub(GetTickCount64()) / 1000,
         Err(_e) => {
             sysinfo_debug!("Failed to compute boot time: {:?}", _e);
             0
@@ -168,14 +168,16 @@ impl SystemExt for System {
             if GetPerformanceInfo(&mut perf_info, size_of::<PERFORMANCE_INFORMATION>() as u32)
                 == TRUE
             {
-                let swap_total = perf_info.PageSize
-                    * perf_info
+                let swap_total = perf_info.PageSize.saturating_mul(
+                    perf_info
                         .CommitLimit
-                        .saturating_sub(perf_info.PhysicalTotal);
-                let swap_used = perf_info.PageSize
-                    * perf_info
+                        .saturating_sub(perf_info.PhysicalTotal),
+                );
+                let swap_used = perf_info.PageSize.saturating_mul(
+                    perf_info
                         .CommitTotal
-                        .saturating_sub(perf_info.PhysicalTotal);
+                        .saturating_sub(perf_info.PhysicalTotal),
+                );
                 self.swap_total = (swap_total / 1000) as u64;
                 self.swap_used = (swap_used / 1000) as u64;
             }

src/apple/system.rs
@@ -257,14 +257,17 @@ impl SystemExt for System {
                 //  * used to hold data that was read speculatively from disk but
                 //  * haven't actually been used by anyone so far.
                 //  */
-                self.mem_available = self.mem_total
-                    - (u64::from(stat.active_count)
-                        + u64::from(stat.inactive_count)
-                        + u64::from(stat.wire_count)
-                        + u64::from(stat.speculative_count)
-                        - u64::from(stat.purgeable_count))
-                        * self.page_size_kb;
-                self.mem_free = u64::from(stat.free_count) * self.page_size_kb;
+                self.mem_available = self
+                    .mem_total
+                    .saturating_sub(
+                        u64::from(stat.active_count)
+                            .saturating_add(u64::from(stat.inactive_count))
+                            .saturating_add(u64::from(stat.wire_count))
+                            .saturating_add(u64::from(stat.speculative_count))
+                            .saturating_sub(u64::from(stat.purgeable_count)),
+                    )
+                    .saturating_mul(self.page_size_kb);
+                self.mem_free = u64::from(stat.free_count).saturating_mul(self.page_size_kb);
             }
         }
     }

src/freebsd/system.rs
@@ -644,12 +644,15 @@ impl SystemInfo {
             }
             let swap =
                 std::slice::from_raw_parts(swap.as_ptr() as *mut libc::kvm_swap, nswap.min(LEN));
-            let (used, total) = swap.iter().fold((0, 0), |(used, total), swap| {
-                (used + swap.ksw_used as u64, total + swap.ksw_total as u64)
+            let (used, total) = swap.iter().fold((0, 0), |(used, total): (u64, u64), swap| {
+                (
+                    used.saturating_add(swap.ksw_used as _),
+                    total.saturating_add(swap.ksw_total as _),
+                )
             });
             (
-                used * self.page_size as u64 / 1_000,
-                total * self.page_size as u64 / 1_000,
+                used.saturating_mul(self.page_size as _) / 1_000,
+                total.saturating_mul(self.page_size as _) / 1_000,
             )
         }
     }
@@ -658,7 +661,7 @@ impl SystemInfo {
         let mut nb_pages: u64 = 0;
         unsafe {
             if get_sys_value(&self.virtual_page_count, &mut nb_pages) {
-                return nb_pages * self.page_size as u64 / 1_000;
+                return nb_pages.saturating_mul(self.page_size as _) / 1_000;
             }
 
             // This is a fallback. It includes all the available memory, not just the one available for
@@ -677,13 +680,15 @@ impl SystemInfo {
             get_sys_value(&self.virtual_active_count, &mut mem_active);
             get_sys_value(&self.virtual_wire_count, &mut mem_wire);
 
-            let mut mem_wire = mem_wire * self.page_size as u64;
+            let mut mem_wire = mem_wire.saturating_mul(self.page_size as _);
             // We need to subtract "ZFS ARC" from the "wired memory" because it should belongs to cache
             // but the kernel reports it as "wired memory" instead...
             if let Some(arc_size) = self.zfs.arc_size() {
                 mem_wire -= arc_size;
             }
-            let used = (mem_active * self.page_size as u64) + mem_wire;
+            let used = mem_active
+                .saturating_mul(self.page_size as _)
+                .saturating_add(mem_wire);
             used / 1_000
         }
     }
@@ -701,9 +706,9 @@ impl SystemInfo {
             get_sys_value(&self.virtual_free_count, &mut free_mem);
             // For whatever reason, buffers_mem is already the right value...
             let free = buffers_mem
-                + (inactive_mem * self.page_size as u64)
-                + (cached_mem * self.page_size as u64)
-                + (free_mem * self.page_size as u64);
+                .saturating_add(inactive_mem.saturating_mul(self.page_size as u64))
+                .saturating_add(cached_mem.saturating_mul(self.page_size as u64))
+                .saturating_add(free_mem.saturating_mul(self.page_size as u64));
             free / 1_000
         }
     }

src/linux/system.rs
@@ -419,7 +419,7 @@ impl SystemExt for System {
                 if let Some(val_str) = iter.next().and_then(|s| s.trim_start().split(' ').next()) {
                     if let Ok(value) = u64::from_str(val_str) {
                         // /proc/meminfo reports KiB, though it says "kB". Convert it.
-                        *field = value * 128 / 125;
+                        *field = value.saturating_mul(128) / 125;
                     }
                 }
             }

src/apple/disk.rs
@@ -56,7 +56,7 @@ impl DiskExt for Disk {
             let mut stat: statfs = mem::zeroed();
             let mount_point_cpath = to_cpath(&self.mount_point);
             if statfs(mount_point_cpath.as_ptr() as *const i8, &mut stat) == 0 {
-                self.available_space = u64::from(stat.f_bsize) * stat.f_bavail;
+                self.available_space = u64::from(stat.f_bsize).saturating_mul(stat.f_bavail);
                 true
             } else {
                 false

src/apple/macos/disk.rs
@@ -172,8 +172,8 @@ fn new_disk(
                 return None;
             }
 
-            total_space = u64::from(stat.f_bsize) * stat.f_blocks;
-            available_space = u64::from(stat.f_bsize) * stat.f_bavail;
+            total_space = u64::from(stat.f_bsize).saturating_mul(stat.f_blocks);
+            available_space = u64::from(stat.f_bsize).saturating_mul(stat.f_bavail);
             let mut vec = Vec::with_capacity(stat.f_fstypename.len());
             for x in &stat.f_fstypename {
                 if *x == 0 {

src/apple/macos/process.rs
@@ -195,9 +195,11 @@ pub(crate) fn compute_cpu_usage(
     time_interval: Option<f64>,
 ) {
     if let Some(time_interval) = time_interval {
-        let total_existing_time = p.old_stime + p.old_utime;
+        let total_existing_time = p.old_stime.saturating_add(p.old_utime);
         if time_interval > 0.000001 && total_existing_time > 0 {
-            let total_current_time = task_info.pti_total_system + task_info.pti_total_user;
+            let total_current_time = task_info
+                .pti_total_system
+                .saturating_add(task_info.pti_total_user);
 
             let total_time_diff = total_current_time.saturating_sub(total_existing_time);
             p.cpu_usage = (total_time_diff as f64 / time_interval * 100.) as f32;
@@ -210,18 +212,20 @@ pub(crate) fn compute_cpu_usage(
         unsafe {
             // This is the "backup way" of CPU computation.
             let time = libc::mach_absolute_time();
-            let task_time =
-                user_time + system_time + task_info.pti_total_user + task_info.pti_total_system;
+            let task_time = user_time
+                .saturating_add(system_time)
+                .saturating_add(task_info.pti_total_user)
+                .saturating_add(task_info.pti_total_system);
 
             let system_time_delta = if task_time < p.old_utime {
                 task_time
             } else {
-                task_time - p.old_utime
+                task_time.saturating_sub(p.old_utime)
             };
             let time_delta = if time < p.old_stime {
                 time
             } else {
-                time - p.old_stime
+                time.saturating_sub(p.old_stime)
             };
             p.old_utime = task_time;
             p.old_stime = time;

src/c_interface.rs
@@ -234,10 +234,9 @@ pub extern "C" fn sysinfo_networks_received(system: CSystem) -> size_t {
     assert!(!system.is_null());
     unsafe {
         let system: Box<System> = Box::from_raw(system as *mut System);
-        let ret = system
-            .networks()
-            .iter()
-            .fold(0, |acc, (_, data)| acc + data.received() as size_t);
+        let ret = system.networks().iter().fold(0, |acc: size_t, (_, data)| {
+            acc.saturating_add(data.received() as size_t)
+        });
         Box::into_raw(system);
         ret
     }
@@ -250,10 +249,9 @@ pub extern "C" fn sysinfo_networks_transmitted(system: CSystem) -> size_t {
     assert!(!system.is_null());
     unsafe {
         let system: Box<System> = Box::from_raw(system as *mut System);
-        let ret = system
-            .networks()
-            .iter()
-            .fold(0, |acc, (_, data)| acc + data.transmitted() as size_t);
+        let ret = system.networks().iter().fold(0, |acc: size_t, (_, data)| {
+            acc.saturating_add(data.transmitted() as size_t)
+        });
         Box::into_raw(system);
         ret
     }

commit 9cfe9493ec73b456ff11c7321d402a163e87235c

fix: fixed Windows boot time
That's a fixup of 87773f69e3a128d3071d9d1702fc127083adf72e, which introduced a wrong operation order

src/windows/system.rs
@@ -73,7 +73,7 @@ unsafe impl<T> Sync for Wrap<T> {}
 
 unsafe fn boot_time() -> u64 {
     match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-        Ok(n) => n.as_secs().saturating_sub(GetTickCount64()) / 1000,
+        Ok(n) => n.as_secs().saturating_sub(GetTickCount64() / 1000),
         Err(_e) => {
             sysinfo_debug!("Failed to compute boot time: {:?}", _e);
             0