cbe8c0ac2e020cd1ccbf645084f8ea469530d6aa
Axel Viala <axel.viala@darnuria.eu>
2022-09-02 16:22:53 +0200

    clippy --fix + cargo fmt pass.

--- tests/code_checkers/docs.rs
+++ tests/code_checkers/docs.rs
@@ -21,14 +21,9 @@ fn to_correct_name(s: &str) -> String {
 }
 
 fn check_md_doc_path(p: &Path, md_line: &str, ty_line: &str) -> bool {
-    let parts = md_line.split("/").collect::<Vec<_>>();
+    let parts = md_line.split('/').collect::<Vec<_>>();
     if let Some(md_name) = parts.last().and_then(|n| n.split(".md").next()) {
-        if let Some(name) = ty_line
-            .split_whitespace()
-            .filter(|s| !s.is_empty())
-            .skip(2)
-            .next()
-        {
+        if let Some(name) = ty_line.split_whitespace().filter(|s| !s.is_empty()).nth(2) {
             if let Some(name) = name
                 .split('<')
                 .next()
@@ -54,7 +49,7 @@ fn check_md_doc_path(p: &Path, md_line: &str, ty_line: &str) -> bool {
     } else {
         show_error(p, &format!("Cannot extract md name from `{}`", md_line));
     }
-    return false;
+    false
 }
 
 fn check_doc_comments_before(p: &Path, lines: &[&str], start: usize) -> bool {
