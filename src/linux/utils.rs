51bdd5249e1b195ac2a29f81930cbc648e09ad03
Guillaume Gomez <guillaume1.gomez@gmail.com>
2022-10-01 15:13:03 +0200

    Greatly simplify linux utils::realpath function

--- src/linux/utils.rs
+++ src/linux/utils.rs
@@ -19,39 +19,12 @@ pub(crate) fn get_all_data<P: AsRef<Path>>(file_path: P, size: usize) -> io::Res
 }
 
 #[allow(clippy::useless_conversion)]
-pub(crate) fn realpath(original: &Path) -> std::path::PathBuf {
-    use libc::{lstat, stat, S_IFLNK, S_IFMT};
-    use std::fs;
-    use std::mem::MaybeUninit;
-    use std::path::PathBuf;
-
-    fn and(x: u32, y: u32) -> u32 {
-        x & y
-    }
-
-    // let ori = Path::new(original.to_str().unwrap());
-    // Right now lstat on windows doesn't work quite well
-    // if cfg!(windows) {
-    //     return PathBuf::from(ori);
-    // }
-    let result = PathBuf::from(original);
-    let mut result_s = result.to_str().unwrap_or("").as_bytes().to_vec();
-    result_s.push(0);
-    let mut buf = MaybeUninit::<stat>::uninit();
-    unsafe {
-        let res = lstat(result_s.as_ptr() as *const _, buf.as_mut_ptr());
-        if res < 0 {
+pub(crate) fn realpath(path: &Path) -> std::path::PathBuf {
+    match std::fs::read_link(path) {
+        Ok(f) => f,
+        Err(_e) => {
+            sysinfo_debug!("failed to get real path for {:?}: {:?}", path, _e);
             PathBuf::new()
-        } else {
-            let buf = buf.assume_init();
-            if and(buf.st_mode.into(), S_IFMT.into()) != S_IFLNK.into() {
-                PathBuf::new()
-            } else {
-                match fs::read_link(&result) {
-                    Ok(f) => f,
-                    Err(_) => PathBuf::new(),
-                }
-            }
         }
     }
 }
