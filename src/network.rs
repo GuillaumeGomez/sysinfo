ce57bfcbd27fed72e5d6d39cea61e7a853c929d6
Guillaume Gomez <guillaume1.gomez@gmail.com>
2023-07-03 19:51:28 +0200

    Improve code to retrieve network interfaces

--- src/network.rs
+++ src/network.rs
@@ -7,11 +7,16 @@ use crate::NetworkData;
 
 /// Interface addresses are OS-independent
 pub(crate) fn refresh_networks_addresses(interfaces: &mut HashMap<String, NetworkData>) {
-    if let Ok(ifa_iterator) = get_interface_address() {
-        for (name, ifa) in ifa_iterator {
-            if let Some(interface) = interfaces.get_mut(&name) {
-                interface.mac_addr = ifa;
+    match get_interface_address() {
+        Ok(ifa_iterator) => {
+            for (name, ifa) in ifa_iterator {
+                if let Some(interface) = interfaces.get_mut(&name) {
+                    interface.mac_addr = ifa;
+                }
             }
+        }
+        Err(_e) => {
+            sysinfo_debug!("refresh_networks_addresses failed: {:?}", _e);
         }
     }
 }
