2fe60934273f56f6d8995486dd162309784dde4b
David Herberth <david.herberth@sentry.io>
2024-03-05 16:27:29 +0100

    remove memory stat code from cgroup mem calculation

--- src/unix/linux/system.rs
+++ src/unix/linux/system.rs
@@ -557,10 +557,6 @@ impl crate::CGroupLimits {
                 limits.free_swap = sys.swap_total.saturating_sub(swap_cur);
             }
 
-            read_table("/sys/fs/cgroup/memory.stat", ' ', |_key, value| {
-                limits.free_memory = limits.free_memory.saturating_sub(value);
-            });
-
             Some(limits)
         } else if let (Some(mem_cur), Some(mem_max)) = (
             // cgroups v1
