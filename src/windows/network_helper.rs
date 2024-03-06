42e6862a839f3879ed8286854da1930dadf2cc26
gongzhengyang <gongzhengyang@bolean.com.cn>
2024-03-06 18:10:38 +0800

    style: change ip_address_from_socket_address if into match

--- src/windows/network_helper.rs
+++ src/windows/network_helper.rs
@@ -152,18 +152,19 @@ fn get_ip_networks(mut prefixes_ptr: *mut IP_ADAPTER_UNICAST_ADDRESS_LH) -> Hash
 /// Converts a Windows socket address to an ip address.
 fn get_ip_address_from_socket_address(socket_address: NonNull<SOCKADDR>) -> Option<IpAddr> {
     let socket_address_family = unsafe { socket_address.as_ref().sa_family };
-
-    if socket_address_family == AF_INET {
-        let socket_address = unsafe { socket_address.cast::<SOCKADDR_IN>().as_ref() };
-        let address = unsafe { socket_address.sin_addr.S_un.S_addr };
-        let ipv4_address = IpAddr::from(address.to_ne_bytes());
-        Some(ipv4_address)
-    } else if socket_address_family == AF_INET6 {
-        let socket_address = unsafe { socket_address.cast::<SOCKADDR_IN6>().as_ref() };
-        let address = unsafe { socket_address.sin6_addr.u.Byte };
-        let ipv6_address = IpAddr::from(address);
-        Some(ipv6_address)
-    } else {
-        None
+    match socket_address_family {
+        AF_INET => {
+            let socket_address = unsafe { socket_address.cast::<SOCKADDR_IN>().as_ref() };
+            let address = unsafe { socket_address.sin_addr.S_un.S_addr };
+            let ipv4_address = IpAddr::from(address.to_ne_bytes());
+            Some(ipv4_address)
+        },
+        AF_INET6 => {
+            let socket_address = unsafe { socket_address.cast::<SOCKADDR_IN6>().as_ref() };
+            let address = unsafe { socket_address.sin6_addr.u.Byte };
+            let ipv6_address = IpAddr::from(address);
+            Some(ipv6_address)
+        },
+        _ => None
     }
 }
