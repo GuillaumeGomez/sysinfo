a06d27ed1b0f16dafba687af36f5778e7c822062
provrb <ethanjamesoliveira@gmail.com>
2025-01-17 19:32:46 +0000

    Update how process names are gathered

--- src/windows/process.rs
+++ src/windows/process.rs
@@ -326,12 +326,8 @@ impl ProcessInner {
     pub(crate) fn from_process_entry(entry: &PROCESSENTRY32W, now: u64) -> Self {
         let pid = Pid::from_u32(entry.th32ProcessID);
         let name = match OsString::from_str(String::from_utf16_lossy(&entry.szExeFile).trim_end_matches('\0')) {
-            Some(name) => name,
-            None => match pid.0 {
-                0 => OsString::from_str("Idle"),
-                4 => OsString::from_str("System"),
-                _ => format!("<no name> Process {process_id}").into(),
-            },
+            Ok(name) => name,
+            Err(_) => format!("<no name> Process {pid}").into(),
         };
         let ppid = {
             if entry.th32ParentProcessID == 0 {
