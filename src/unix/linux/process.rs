cda77762bd167e6269196ee7260679d47acf037c
psengrith <psengrith.dev@gmail.com>
2025-02-20 14:29:39 +0700

    remove incorrect PartialEq trait on FileCounter

--- src/unix/linux/process.rs
+++ src/unix/linux/process.rs
@@ -1039,7 +1039,7 @@ impl PartialOrd for FileCounter {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         match self.0.metadata() {
             Ok(m1) => match other.0.metadata() {
-                Ok(m2) => m1.mtime_nsec().cmp(&m2.mtime_nsec()),
+                Ok(m2) => Some(m1.mtime_nsec().cmp(&m2.mtime_nsec())),
                 _ => None,
             },
             _ => None,
