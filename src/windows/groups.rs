3ccc45bc7a9ff04b95a91c558614e4b2b83304ec
Guillaume Gomez <guillaume1.gomez@gmail.com>
2025-04-30 22:30:55 +0200

    Improve group retrieval code in Windows

--- src/windows/groups.rs
+++ src/windows/groups.rs
@@ -1,15 +1,12 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
 use crate::sys::utils::to_utf8_str;
-use crate::windows::sid::Sid;
 use crate::{Gid, Group, GroupInner};
 
 use std::ptr::null_mut;
-use windows::core::PCWSTR;
-use windows::Win32::Foundation::ERROR_MORE_DATA;
+use windows::Win32::Foundation::{ERROR_MORE_DATA, ERROR_SUCCESS};
 use windows::Win32::NetworkManagement::NetManagement::{
-    NERR_Success, NetApiBufferFree, NetGroupEnum, NetGroupGetInfo, GROUP_INFO_0, GROUP_INFO_3,
-    MAX_PREFERRED_LENGTH,
+    NetApiBufferFree, NetQueryDisplayInformation, MAX_PREFERRED_LENGTH, NET_DISPLAY_GROUP,
 };
 
 impl GroupInner {
@@ -26,9 +23,9 @@ impl GroupInner {
     }
 }
 
-struct NetApiBuffer<T>(*mut T);
+struct NetApiBuffer(*mut NET_DISPLAY_GROUP);
 
-impl<T> Drop for NetApiBuffer<T> {
+impl Drop for NetApiBuffer {
     fn drop(&mut self) {
         if !self.0.is_null() {
             unsafe { NetApiBufferFree(Some(self.0.cast())) };
@@ -36,82 +33,52 @@ impl<T> Drop for NetApiBuffer<T> {
     }
 }
 
-impl<T> Default for NetApiBuffer<T> {
+impl Default for NetApiBuffer {
     fn default() -> Self {
         Self(null_mut())
     }
 }
 
-impl<T> NetApiBuffer<T> {
-    pub fn inner_mut(&mut self) -> &mut *mut T {
-        assert!(self.0.is_null());
-        &mut self.0
-    }
-
-    pub unsafe fn inner_mut_as_bytes(&mut self) -> &mut *mut u8 {
-        // https://doc.rust-lang.org/std/mem/fn.transmute.html
-        // Turning an &mut T into an &mut U:
-        &mut *(self.inner_mut() as *mut *mut T as *mut *mut u8)
+impl NetApiBuffer {
+    pub fn inner_mut(&mut self) -> *mut *mut NET_DISPLAY_GROUP {
+        &mut self.0 as *mut _
     }
 }
 
 pub(crate) fn get_groups(groups: &mut Vec<Group>) {
     groups.clear();
 
-    let mut resume_handle: usize = 0;
     unsafe {
+        let mut i = 0;
+        let mut nb_entries = 0;
         loop {
-            let mut buffer: NetApiBuffer<GROUP_INFO_0> = Default::default();
-            let mut nb_read = 0;
-            let mut total = 0;
-            let status = NetGroupEnum(
-                PCWSTR::null(),
-                0,
-                buffer.inner_mut_as_bytes(),
+            let mut buff = NetApiBuffer::default();
+            let res = NetQueryDisplayInformation(
+                None,
+                3, // Level. Here, group account information.
+                i, // If it's not the first iteration, frm which index we want to start from.
+                1000,
                 MAX_PREFERRED_LENGTH,
-                &mut nb_read,
-                &mut total,
-                Some(&mut resume_handle),
+                &mut nb_entries,
+                buff.inner_mut() as *mut _,
             );
-            if status == NERR_Success || status == ERROR_MORE_DATA.0 {
-                let entries = std::slice::from_raw_parts(buffer.0, nb_read as _);
-                for entry in entries {
-                    if entry.grpi0_name.is_null() {
-                        continue;
-                    }
-
-                    let mut group: NetApiBuffer<GROUP_INFO_3> = Default::default();
-                    if NetGroupGetInfo(
-                        PCWSTR::null(),
-                        PCWSTR::from_raw(entry.grpi0_name.as_ptr()),
-                        3,
-                        group.inner_mut_as_bytes(),
-                    ) == NERR_Success
-                    {
-                        if let Some(_sid) = Sid::from_psid((*group.0).grpi3_group_sid) {
-                            // Get the account name from the SID (because it's usually
-                            // a better name), but fall back to the name we were given
-                            // if this fails.
-                            let name = to_utf8_str(entry.grpi0_name);
-                            groups.push(Group {
-                                inner: GroupInner::new(Gid(0), name),
-                            });
-                        }
-                    }
-                }
-            } else {
-                sysinfo_debug!(
-                    "NetGroupEnum error: {}",
-                    if status == windows::Win32::Foundation::ERROR_ACCESS_DENIED.0 {
-                        "access denied"
-                    } else if status == windows::Win32::Foundation::ERROR_INVALID_LEVEL.0 {
-                        "invalid level"
-                    } else {
-                        "unknown error"
-                    }
-                );
+            if res != ERROR_SUCCESS.0 && res != ERROR_MORE_DATA.0 {
+                sysinfo_debug!("NetQueryDisplayInformation failed: {res:?}");
+                break;
             }
-            if status != ERROR_MORE_DATA.0 {
+            let mut p = buff.0;
+            for _ in 0..nb_entries {
+                let name = to_utf8_str((*p).grpi3_name);
+                groups.push(Group {
+                    inner: GroupInner::new(Gid((*p).grpi3_group_id), name),
+                });
+                //
+                // If there is more data, set the index.
+                //
+                i = (*p).grpi3_next_index;
+                p = p.add(1);
+            }
+            if res != ERROR_MORE_DATA.0 {
                 break;
             }
         }
