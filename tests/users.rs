24de2602179f07d5f0cb8eb53f6cbbf7ec3edd22
Guillaume Gomez <guillaume1.gomez@gmail.com>
2025-05-01 18:16:10 +0200

    Add test for `Groups`

--- tests/users.rs
+++ tests/users.rs
@@ -14,14 +14,29 @@
 fn test_users() {
     use sysinfo::Users;
 
-    if sysinfo::IS_SUPPORTED_SYSTEM {
-        let mut users = Users::new();
-        assert_eq!(users.iter().count(), 0);
-        users.refresh();
-        assert!(users.iter().count() > 0);
-        let count = users.first().unwrap().groups().iter().len();
-        for _ in 1..10 {
-            assert!(users.first().unwrap().groups().iter().len() == count)
-        }
+    if !sysinfo::IS_SUPPORTED_SYSTEM {
+        return;
     }
+    let mut users = Users::new();
+    assert_eq!(users.iter().count(), 0);
+    users.refresh();
+    assert!(users.iter().count() > 0);
+    let count = users.first().unwrap().groups().iter().len();
+    for _ in 1..10 {
+        assert!(users.first().unwrap().groups().iter().len() == count)
+    }
+}
+
+// This test ensures that there are actually groups listed, in particular for Windows.
+#[cfg(feature = "user")]
+#[test]
+fn test_groups() {
+    use sysinfo::Groups;
+
+    if !sysinfo::IS_SUPPORTED_SYSTEM {
+        return;
+    }
+    let mut groups = Groups::new();
+    groups.refresh();
+    assert!(groups.list().len() > 1);
 }
