420408d8cbc8aa2e0101b09bf11d72c524b3bcbe
Skgland <bennet.blessmann+github@googlemail.com>
2026-03-26 23:15:07 +0100

    fix unknown

--- src/unknown/network.rs
+++ src/unknown/network.rs
@@ -1,6 +1,6 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
-use crate::{IpNetwork, MacAddr, NetworkData};
+use crate::{InterfaceOperationalState, IpNetwork, MacAddr, NetworkData};
 
 use std::collections::HashMap;
 
