deb4854fbe4fd8383884c43aaa4537b01ac9b6fa
Guillaume Gomez <contact@guillaume-gomez.fr>
2026-05-29 00:09:18 +0200

    Unix: Fix retrieval of `Network::mac_addr`

--- tests/network.rs
+++ tests/network.rs
@@ -2,15 +2,30 @@
 
 // This test is used to ensure that the networks are not loaded by default.
 
-#[cfg(feature = "network")]
+#![cfg(feature = "network")]
+
+use sysinfo::Networks;
+
 #[test]
 fn test_networks() {
-    use sysinfo::Networks;
-
-    if sysinfo::IS_SUPPORTED_SYSTEM {
-        let mut n = Networks::new();
-        assert_eq!(n.iter().count(), 0);
-        n.refresh(false);
-        assert!(n.iter().count() > 0);
+    if !sysinfo::IS_SUPPORTED_SYSTEM {
+        return;
     }
+    let mut networks = Networks::new();
+    assert_eq!(networks.list().len(), 0);
+    networks.refresh(false);
+    assert_ne!(networks.list().len(), 0);
+}
+
+#[test]
+fn test_mac_addr() {
+    if !sysinfo::IS_SUPPORTED_SYSTEM {
+        return;
+    }
+    let mut networks = Networks::new();
+    networks.refresh(false);
+    assert_ne!(networks.list().len(), 0);
+    networks
+        .iter()
+        .any(|(_, n)| !n.mac_address().is_unspecified());
 }
